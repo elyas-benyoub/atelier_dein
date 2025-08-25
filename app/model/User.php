d7a75e27f24a5c0c9f8433f82bb694ed0d9076a6
Daria Plishanova <daria.plishanova@laplateforme.io>
2025-08-25 15:23:59 +0200

    class user ->register + connect

--- app/model/User.php
+++ app/model/User.php
@@ -9,8 +9,26 @@ class User {
     private $id;
 
     public function connect($login, $password) {
-        $this->login = $login;
-        $this->password = $password;
+        $this -> login = $login;
+        $this -> password = $password;
+
+        return [
+            $login
+        ];
+    }
+
+    public function register($login, $password, $email, $firstname, $lastname) {
+        $this -> login = $login;
+        $this -> password = $password;
+        $this -> email = $email;
+        $this -> firstname = $firstname;
+        $this -> lastname = $lastname;
+        
+        return [
+            $login,
+            $email,
+            $firstname,
+            $lastname
+        ];
     }
 }
-
