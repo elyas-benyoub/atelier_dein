515e8a6e5dbc30dafc45a7c1206f209175728ca0
nesrine-benlarbi <nesrine.benlarbi@laplateforme.io>
2025-08-26 14:36:58 +0200

    trjlyrgtrljh

--- app/controller/connexionController.php
+++ app/controller/connexionController.php
@@ -1,10 +1,30 @@
 <?php
+
+/* on requiert une seule fois; les deux point c'est 
+** l'action de revenir en arrière sur le dossier parent entre slash
+** et appeler le fichier tous ça entre guillemet double
+*/
+
 require_once "../model/User.php";
+
 $login = $_GET['username'];
 $password = $_GET['password'];
+
+/*
+** on enregistre les données du formulaire
+** à travers la variable GET qui sert de véhicule
+*/
+
 $user = new User();
+/*on instancie un nouvel utilisateur avec la class User*/
+
 $data = $user->connect($login, $password);
-var_dump($data);
+/*
+** on appelle la methode (fonction) connect() pour connecter l'utilisateur
+** la methode renvoie des donnees (datas, message...).
+*/
+
+var_dump($data); // afficher et verifier les donnees dans le navigateur
 ?>
 
 
