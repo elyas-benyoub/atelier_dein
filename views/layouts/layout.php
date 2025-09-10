196116c8d973b6b6373341a936a86d4b53a8db17
ihsankhadem <ihsan.khadem@laplateforme.io>
2025-09-10 10:47:34 +0200

    code adminuser

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -21,33 +21,42 @@
                 <li><a href="<?php echo url('home/about'); ?>">À propos</a></li>
                 <li><a href="<?php echo url('home/contact'); ?>">Contact</a></li>
 
-                <?php if (is_logged_in()): ?>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle">
-                            <?php if (!empty($_SESSION['user_profile'])): ?>
-                                <img src="<?php echo url('uploads/profiles/' . $_SESSION['user_profile']); ?>" 
-                                    alt="Profile" class="profile-pic">
-                            <?php else: ?>
-                                <i class="fas fa-user-circle fa-2x"></i>
-                            <?php endif; ?>
-                            <?php echo e($_SESSION['user_name']); ?>
-                            <i class="fas fa-caret-down"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <?php if (is_admin()): ?>
-                                <li><a href="<?php echo url('media/add_book'); ?>"><i class="fas fa-book"></i> Ajouter un livre</a></li>
-                                <li><a href="<?php echo url('media/add_movie'); ?>"><i class="fas fa-film"></i> Ajouter un film</a></li>
-                                <li><a href="<?php echo url('media/add_game'); ?>"><i class="fas fa-gamepad"></i> Ajouter un jeu</a></li>
-                                <li class="divider"></li>
-                            <?php endif; ?>
-                            <li><a href="<?php echo url('auth/logout'); ?>"><i class="fas fa-sign-out-alt"></i> Déconnexion</a></li>
-                        </ul>
-                    </li>
 
-                <?php else: ?>
-                    <li><a href="<?php echo url('auth/login'); ?>">Connexion</a></li>
-                    <li><a href="<?php echo url('auth/register'); ?>">Inscription</a></li>
-                <?php endif; ?>
+
+
+                    <?php if (is_logged_in()): ?>
+                        <li class="dropdown"> 
+                            <button type="button" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">
+                                <?php if (!empty($_SESSION['user_profile'])): ?>
+                                    <img src="<?php echo url('uploads/profiles/' . $_SESSION['user_profile']); ?>" 
+                                        alt="Profile" class="profile-pic">
+                                <?php else: ?>
+                                    <i class="fas fa-user-circle fa-2x"></i>
+                                <?php endif; ?>
+                                <span class="nav-username"><?= e($_SESSION['user_name']); ?></span>
+                                <i class="fas fa-caret-down caret-icon" aria-hidden="true"></i>
+                            </button>
+
+                            <ul class="dropdown-menu" role="menu" aria-label="Menu utilisateur">
+                                <?php if (is_admin()): ?>
+                                    <li><a href="<?php echo url('media/add_book'); ?>"><i class="fas fa-book"></i> Ajouter un livre</a></li>
+                                    <li><a href="<?php echo url('media/add_movie'); ?>"><i class="fas fa-film"></i> Ajouter un film</a></li>
+                                    <li><a href="<?php echo url('media/add_game'); ?>"><i class="fas fa-gamepad"></i> Ajouter un jeu</a></li>
+                                    <li class="divider" aria-hidden="true"></li>
+                                <?php endif; ?>
+                                <li><a href="<?php echo url('auth/logout'); ?>"><i class="fas fa-sign-out-alt"></i> Déconnexion</a></li>
+                            </ul>
+                        </li>
+                    <?php else: ?>
+                        <li><a href="<?php echo url('auth/login'); ?>">Connexion</a></li>
+                        <li><a href="<?php echo url('auth/register'); ?>">Inscription</a></li>
+                    <?php endif; ?>
+
+
+
+
+
+
             </ul>
         </nav>
     </header>
